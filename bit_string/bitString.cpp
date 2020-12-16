1bf6e2acb621b3c62fd937550bf5560118767e8d
Chiro <sachinsinghroxx@gmail.com>
2020-12-16 16:01:03 +0530

    added bit string

--- bit_string/bitString.cpp
+++ bit_string/bitString.cpp
@@ -2,11 +2,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+long int power(long long  b,long long p, long long md){
+    long long ans = 1;
+    for (long long i=0;i<p;i++)ans = ans*b%md;
+    return ans;
+}
+
 int main(){
     long long n;
     cin>>n;
-    unsigned long long int p= pow(2,n);
+    cout<<power(2,n,1e9+7);
     // unsigned long P = (unsigned  long)p;
-    cout<<p<<endl;
+    // cout<<p<<endl;
     return 0;
 }
