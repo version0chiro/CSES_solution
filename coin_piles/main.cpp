9a974e2fb14c0a7bd76b8806adf75e7b169a69ed
Chiro <sachinsinghroxx@gmail.com>
2021-01-09 01:39:02 +0530

    changed logic

--- coin_piles/main.cpp
+++ coin_piles/main.cpp
@@ -9,8 +9,11 @@ int main(){
         long long a,b;
         cin>>a>>b;
         
-        if (2*a-b >=0 && ((2*a-b)%3==0) && 2*b-a>=0 && ((2*b-a)%3==0)){
-           cout<<"YES"<<endl; 
+        // if (2*a-b >=0 && ((2*a-b)%3==0) && 2*b-a>=0 && ((2*b-a)%3==0)){
+        //    cout<<"YES"<<endl; 
+        // }
+        if(a<=2*b && b<=2*a && (a+b)%3==0){
+            cout<<"YES"<<endl;
         }
         else{
             cout<<"NO"<<endl;
