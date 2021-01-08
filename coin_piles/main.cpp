5e99435908b24d5b1239ea33fb0c6d4b039d150b
Chiro <sachinsinghroxx@gmail.com>
2021-01-09 01:04:48 +0530

    solved

--- coin_piles/main.cpp
+++ coin_piles/main.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main(){
-
+    long long tries;
+    cin >> tries;
+    while(tries--){
+        long long a,b;
+        cin>>a>>b;
+        
+        if (2*a-b >=0 && ((2*a-b)%3==0) && 2*b-a>=0 && ((2*b-a)%3==0)){
+           cout<<"YES"<<endl; 
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
     return 0;
 }
