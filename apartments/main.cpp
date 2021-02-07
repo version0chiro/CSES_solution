ee161fdf1cc2d1c70a5b1b8b495e4dd84aed1a75
Chiro <sachinsinghroxx@gmail.com>
2021-02-08 01:22:29 +0530

    redid distinct with sets still not great speed

---
+++ apartments/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define FOR(x) for(int i=0;i<x;i++)
+using namespace std;
+
+int main(){
+    int n,m,k;
+    cin >> n >> m >> k;
+    int applicanDesire[n];
+    int apartmentSize[n];
+    FOR(n){cin >> applicanDesire[i];}
+    FOR(m){cin >> apartmentSize[i];}
+    int count = 0;
+    FOR(n){
+        if()
+        
+    }
+
+    return 0;
+}
