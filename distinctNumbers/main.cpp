690d633be72499c0b22f8278a62bcd3b8bb79d37
Chiro <sachinsinghroxx@gmail.com>
2021-02-07 21:38:51 +0530

    went back to old algorithm for better speed

--- distinctNumbers/main.cpp
+++ distinctNumbers/main.cpp
@@ -9,26 +9,16 @@ int main(){
     int n;
     cin >> n;
     int arr[n];
-    map<int, int> count;
     Loop(n){
         cin >> arr[i];
-        if(count[arr[i]]==arr[i]){
-            count[arr[i]]++;            
-        }
-        else{
-            count[arr[i]] = arr[i];
-        }
-
     }
-    
-    cout<<count.size()<<"\n";
-    // sort(arr,arr+n);
-    // int count = 0;3
-    // Loop(n){
-    //     if(arr[i]!=arr[i+1]){
-    //         count++;
-    //     }
-    // }
-    // cout <<"\n"<<count;
+    sort(arr,arr+n);
+    int count = 0;
+    Loop(n){
+        if(arr[i]!=arr[i+1]){
+            count++;
+        }
+    }
+    cout <<"\n"<<count;
     return 0;
 }
