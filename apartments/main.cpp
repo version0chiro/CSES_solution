e233b5c6ca5183a2b2ba3d20977a2cd385ec69ae
Chiro <sachinsinghroxx@gmail.com>
2021-02-08 02:08:52 +0530

    done with while loop and swiping of array

--- apartments/main.cpp
+++ apartments/main.cpp
@@ -1,20 +1,40 @@
 #include <bits/stdc++.h>
 
-#define FOR(x) for(int i=0;i<x;i++)
+#define FOR(x) for(long long i=0;i<x;i++)
+#define FOR2(x) for(long long j=0;j<x;j++)
 using namespace std;
 
 int main(){
-    int n,m,k;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long n,m,k,papr=0,papp=0,t,working=0;
+    vector<long> apr,app;
     cin >> n >> m >> k;
-    int applicanDesire[n];
-    int apartmentSize[n];
-    FOR(n){cin >> applicanDesire[i];}
-    FOR(m){cin >> apartmentSize[i];}
-    int count = 0;
     FOR(n){
-        if()
-        
+        cin >>t;
+        apr.push_back(t);
     }
+    FOR2(m){
+        cin >>t;
+        app.push_back(t);
+    }
+    sort(app.begin(),app.end());
+    sort(apr.begin(),apr.end());
+    while(papr<n && papp<m){
+        cout<<"=====";
+        cout <<"The comparision is between "<<apr[papr]<<" and  "<<app[papp]<<"\n";
+        if(abs(apr[papr]-app[papp])<=k){
+            working++;
+            papr++;
+            papp++;
+        }else if(apr[papr]<app[papp]-k){
+            papr++;
+        }
+        else{
+            papp++;
+        }
 
-    return 0;
+    }
+    cout<<working;
+   return 0;
 }
