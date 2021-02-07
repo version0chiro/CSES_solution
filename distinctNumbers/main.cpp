6ee11e1cd76f64758b9236b9abe38849cccfd22a
Chiro <sachinsinghroxx@gmail.com>
2021-02-07 21:10:06 +0530

    redid distinct with map

--- distinctNumbers/main.cpp
+++ distinctNumbers/main.cpp
@@ -9,16 +9,26 @@ int main(){
     int n;
     cin >> n;
     int arr[n];
+    map<int, int> count;
     Loop(n){
         cin >> arr[i];
-    }
-    sort(arr,arr+n);
-    int count = 0;
-    Loop(n){
-        if(arr[i]!=arr[i+1]){
-            count++;
+        if(count[arr[i]]==arr[i]){
+            count[arr[i]]++;            
         }
+        else{
+            count[arr[i]] = arr[i];
+        }
+
     }
-    cout <<"\n"<<count;
+    
+    cout<<count.size()<<"\n";
+    // sort(arr,arr+n);
+    // int count = 0;3
+    // Loop(n){
+    //     if(arr[i]!=arr[i+1]){
+    //         count++;
+    //     }
+    // }
+    // cout <<"\n"<<count;
     return 0;
 }
